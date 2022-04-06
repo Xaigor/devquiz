48739cbc4a391f4cdbcec4ae9420021771881398
Xaigor <xaigor@hotmail.com>
2022-04-06 17:35:26 -0300

    Continuação

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:DevQuiz/core/app_gradients.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
@@ -10,30 +11,36 @@ class AppBarWidget extends PreferredSize {
       decoration: BoxDecoration(gradient: AppGradients.linear ),
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Text.rich(TextSpan(
-              text: "Olá, ",
-              style: AppTextStyles.title,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                TextSpan(
-                  text: "Igor teste",
-                  style: AppTextStyles.titleBold
+                Text.rich(TextSpan(
+                  text: "Olá, ",
+                  style: AppTextStyles.title,
+                  children: [
+                    TextSpan(
+                      text: "Igor teste",
+                      style: AppTextStyles.titleBold
+                    )
+                  ]
+                )),
+                Container(
+                  width: 58,
+                  height: 58,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(image: NetworkImage("https://avatars.githubusercontent.com/u/42824235?v=4"),
+                    ),
+                  ),
                 )
-              ]
-            )),
-            Container(
-              width: 58,
-              height: 58,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(image: NetworkImage("https://avatars.githubusercontent.com/u/42824235?v=4"),
-                ),
+              ],
               ),
-            )
+              ScoreCardWidget()
           ],
-          ),
+        ),
       ),
     ),
   );

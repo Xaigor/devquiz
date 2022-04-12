aea9f1c7f33f601f0262da49590d992f4f26cc47
Xaigor <xaigor@hotmail.com>
2022-04-12 17:12:02 -0300

    pequenos ajustes no quiz widget

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,35 +1,42 @@
 import 'package:DevQuiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:DevQuiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:DevQuiz/challenge/widgets/quiz/quiz_wiget.dart';
+import 'package:DevQuiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
 class ChallengePage extends StatefulWidget {
-  const ChallengePage({ Key? key }) : super(key: key);
+  final List<QuestionModel> questions;
+  const ChallengePage({Key? key, required this.questions}) : super(key: key);
 
   @override
   State<ChallengePage> createState() => _ChallengePageState();
 }
 
 class _ChallengePageState extends State<ChallengePage> {
+
+  _ChallengePageState(this.questions);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(86),
         child: SafeArea(
-          top: true, 
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              IconButton(onPressed: (){
-                Navigator.pop(context);
-              },
-              icon: Icon(Icons.close)),
-              QuestionIndicatorWidget(),
-            ],
-          )),
+            top: true,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(Icons.close)),
+                QuestionIndicatorWidget(),
+              ],
+            )),
       ),
-      body: QuizWidget(title: "O que o Flutter faz em sua totalidade?"),
+      body: QuizWidget(
+        question: widget.questions[0],
+      ),
       bottomNavigationBar: SafeArea(
         bottom: true,
         child: Padding(
@@ -37,9 +44,14 @@ class _ChallengePageState extends State<ChallengePage> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Expanded(child: NextButtonWidget.white(label: "Pular", onTap: () {})),
-              SizedBox(width: 7,),
-              Expanded(child: NextButtonWidget.green(label: "Confirmar", onTap: () {})),
+              Expanded(
+                  child: NextButtonWidget.white(label: "Pular", onTap: () {})),
+              SizedBox(
+                width: 7,
+              ),
+              Expanded(
+                  child:
+                      NextButtonWidget.green(label: "Confirmar", onTap: () {})),
             ],
           ),
         ),
--- lib/challenge/widgets/quiz/quiz_wiget.dart
+++ lib/challenge/widgets/quiz/quiz_wiget.dart
@@ -1,10 +1,11 @@
 import 'package:DevQuiz/challenge/widgets/answer/answer_widget.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
 class QuizWidget extends StatelessWidget {
-  final String title;
-  const QuizWidget({Key? key, required this.title}) : super(key: key);
+  final QuestionModel question;
+  const QuizWidget({Key? key, required this.question}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,13 +14,16 @@ class QuizWidget extends StatelessWidget {
         children: [
           SizedBox(height: 64,),
           Text(
-            title,
+            question.title,
             style: AppTextStyles.heading,
           ),
           SizedBox(height: 24),
-          AnswerWidget(isRight: true, isSelected: true, title: "Possibilita a criação de aplicativos compilados nativamente"),
-          AnswerWidget(title: "Possibilita a criação de aplicativos compilados nativamente"),
-          AnswerWidget(title: "Possibilita a criação de aplicativos compilados nativamente"),
+          ...question.answers.map(
+            (e) => AnswerWidget(
+             isRight: e.isRight,
+            title: e.title,
+             ),
+          )
         ],
       ),
     );

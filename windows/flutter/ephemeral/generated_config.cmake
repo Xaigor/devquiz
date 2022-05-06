9aa501994219c2f9a0d3befda8f65c18a7857010
Xaigor <xaigor@hotmail.com>
2022-05-06 14:36:44 -0300

    gitignore funcionando

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Desenvolvimento\\Visual-Studio-Code-workspace\\devquiz" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Desenvolvimento\\Visual-Studio-Code-workspace\\devquiz"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Desenvolvimento\\Visual-Studio-Code-workspace\\devquiz\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Desenvolvimento\\Visual-Studio-Code-workspace\\devquiz"
+  "FLUTTER_TARGET=C:\\Desenvolvimento\\Visual-Studio-Code-workspace\\devquiz\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Desenvolvimento\\Visual-Studio-Code-workspace\\devquiz\\.dart_tool\\package_config.json"
+)
